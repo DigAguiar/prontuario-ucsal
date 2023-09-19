35b590e93733dbd0283553fa24863e8c8759a204
Diogo Ribeiro <diogoribeiro698@gmail.com>
2023-09-19 21:12:40 +0000

    add: entities

---
+++ eletronico/src/main/java/com/prontuario/eletronico/entities/UserModel.java
@@ -0,0 +1,51 @@
+package com.prontuario.eletronico.entities;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String email;
+    private String password;
+    private List<Role> roles;
+
+    public UserModel() {
+    }
+    public UserModel(Integer id, String email, String password, List<Role> roles) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public List<Role> getRoles() {
+        return roles;
+    }
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+}
