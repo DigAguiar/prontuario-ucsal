5ba1ebbf494ea54d87e3aa6f8a5f7cec726c0f5a
Diogo Ribeiro <diogoribeiro698@gmail.com>
2023-09-19 19:50:32 +0000

    add: projeto spring

---
+++ eletronico/src/test/java/com/prontuario/eletronico/EletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prontuario.eletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
