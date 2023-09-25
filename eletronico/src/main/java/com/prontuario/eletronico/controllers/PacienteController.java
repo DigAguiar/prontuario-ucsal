c862edc3c0ef3280fa98f5f2674533f2d2a5c4f3
Diogo Ribeiro <diogoribeiro698@gmail.com>
2023-09-25 22:33:44 +0000

    add: pacientService

--- eletronico/src/main/java/com/prontuario/eletronico/controllers/PacienteController.java
+++ eletronico/src/main/java/com/prontuario/eletronico/controllers/PacienteController.java
@@ -1,32 +1,19 @@
 package com.prontuario.eletronico.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.prontuario.eletronico.DTOs.CadastroDadosPessoaisDTO;
-import com.prontuario.eletronico.entities.Log;
-import com.prontuario.eletronico.entities.Pacient;
-import com.prontuario.eletronico.entities.UserModel;
-import com.prontuario.eletronico.repositories.LogRepository;
-import com.prontuario.eletronico.repositories.PacienteRepository;
-import com.prontuario.eletronico.repositories.UserRepository;
+import com.prontuario.eletronico.services.PacienteService;
 
 @Controller
 public class PacienteController {
 
     @Autowired
-    private PacienteRepository pacienteRepository;
-
-    @Autowired
-    private LogRepository logRepository;
-
-    @Autowired
-    private UserRepository userRepository;
+    private PacienteService pacienteService;
 
     @GetMapping("/cadastarDados")
     public String cadastarDadosPessoais() {
@@ -36,23 +23,7 @@ public class PacienteController {
     @PostMapping("/cadastarDados")
     public String cadastarDadosPessoaisPOST(@ModelAttribute CadastroDadosPessoaisDTO request) {
 
-        var paciente = new Pacient();
-        var log = new Log();
-        var user = new UserModel();
-
-        paciente.setNome("Neymar");
-        paciente = pacienteRepository.save(paciente);
-
-        // userLogged?
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = authentication.getName();
-
-        user = userRepository.findByEmail(username).get();
-
-        log.setIdUser(user.getId());
-        log.setIdPaciente(paciente.getId());
-        log.setAction("criar dados pessoais");
-        logRepository.save(log);
+        pacienteService.cadastrarDadosPessoais(request);
 
         return "paciente/cadastroDados";
     }
---
+++ eletronico/src/main/java/com/prontuario/eletronico/services/PacienteService.java
@@ -0,0 +1,51 @@
+package com.prontuario.eletronico.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.prontuario.eletronico.DTOs.CadastroDadosPessoaisDTO;
+import com.prontuario.eletronico.entities.Log;
+import com.prontuario.eletronico.entities.Pacient;
+import com.prontuario.eletronico.entities.UserModel;
+import com.prontuario.eletronico.repositories.LogRepository;
+import com.prontuario.eletronico.repositories.PacienteRepository;
+import com.prontuario.eletronico.repositories.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class PacienteService {
+    
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @Autowired
+    private LogRepository logRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void cadastrarDadosPessoais(CadastroDadosPessoaisDTO request){
+        var paciente = new Pacient();
+        var log = new Log();
+        var user = new UserModel();
+
+        paciente.setNome("Neymar");
+        paciente = pacienteRepository.save(paciente);
+
+        // userLogged?
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        user = userRepository.findByEmail(username).get();
+
+        log.setIdUser(user.getId());
+        log.setIdPaciente(paciente.getId());
+        log.setAction("criar dados pessoais");
+        logRepository.save(log);
+    }
+
+}
